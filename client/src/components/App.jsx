6c5b01624caa9fe000f11274115d35d071b370b5
danika-gray <danika.partridge@gmail.com>
2022-04-27 10:57:01 -0700

    implement hardcoded movie list in App.jsx

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,42 @@
 import React from 'react';
+import MovieList from './MovieList.js';
 
-const App = (props) => (
-  <div>Hello World!</div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: [
+        {title: 'Mean Girls'},
+        {title: 'Hackers'},
+        {title: 'The Grey'},
+        {title: 'Sunshine'},
+        {title: 'Ex Machina'},
+      ]
+    }
+  }
+
+  render() {
+    return (
+    <div id = 'Movie List'>
+      <ul>
+        <li>{movies[0].title}</li>
+        <li>{movies[1].title}</li>
+        <li>{movies[2].title}</li>
+        <li>{movies[3].title}</li>
+        <li>{movies[4].title}</li>
+      </ul>
+    </div>
+    );
+  }
+};
+
+var movies = [
+  {title: 'Mean Girls'},
+  {title: 'Hackers'},
+  {title: 'The Grey'},
+  {title: 'Sunshine'},
+  {title: 'Ex Machina'},
+];
 
 export default App;
+
