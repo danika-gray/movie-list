88b7cfe87cb16b353a311163a37607fb9b38abdf
danika-gray <danika.partridge@gmail.com>
2022-04-27 17:25:02 -0700

    implement watched button toggle for movie list item

--- client/src/components/MovieListItem.jsx
+++ client/src/components/MovieListItem.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
 
-const MovieListItem = ({movie}) => {
-  return (
-    <p>{movie.title} <span> <button>watched</button> </span></p>
-  )
+// const MovieListItem = ({movie}) => {
+//   return (
+//     <p>{movie.title} <span> <button>watched?</button> </span></p>
+//   )
+// }
+
+class MovieListItem extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      button: 'not watched'
+    }
+    this.toggleButton = this.toggleButton.bind(this);
+  }
+
+  toggleButton(event) {
+    if (this.state.button === 'not watched') {
+      this.setState({
+        button: 'watched'
+      })
+    } else {
+      this.setState({
+        button: 'not watched'
+      })
+    }
+  }
+
+  render() {
+    return (
+      <p>{this.props.movie.title} <span> <button onClick={this.toggleButton}>{this.state.button}</button> </span></p>
+    )
+  }
 }
 
 export default MovieListItem;
